7088e1b918e753992a316c8ed7441af2702d642f
Rehmat Alam <contact@rehmat.works>
2022-08-09 15:50:05 +0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="intro">
+      <h1>Nanga Parbat Tours</h1>
+      <p>
+        Hi 👋! Are you interested in visiting the killer mountain? Book a tour with us and you will never regret it.
+      </p>
+
+      <p>
+        With our expert tour guides, you will enjoy the each and every moment of your adventure. Get closer to nature and spend some of your life's
+        best moments.
+      </p>
+    </div>
+    <FeedbackForm />
+  </main>
+</template>
+
+<style>
+* {
+  font-family: 'Helvetica', sans-serif;
+}
+.intro {
+  min-width: 400px;
+  padding: 10px 40px;
+  width: 60%;
+}
+</style>
---
+++ pages/success.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Thanks for getting in touch!</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
